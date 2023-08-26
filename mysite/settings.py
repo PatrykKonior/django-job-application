6c843c63857d73bbb579c8fa90baca6d178ec197
Patryk <patrykkonior4@gmail.com>
2023-08-26 12:31:04 +0200

    Send email to receiver

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email sanding
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'HERE GOES YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'HERE GOES YOUR PASSWORD TO EMAIL ACCOUNT'
